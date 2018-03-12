efeae4777f2b9a66365461765cb09125f17c3bd2
sanshika807 <sanshika807@gmail.com>
2018-03-12 22:53:18 +0530

    first commit

---
+++ app/src/main/java/bharat/otouch/www/solutionexecutive/UserData.java
@@ -0,0 +1,11 @@
+package bharat.otouch.www.solutionexecutive;
+
+
+
+public class UserData {
+    public String UserQuery;
+    public String UserName;
+    public String UserMobile;
+    public String UserEmail;
+    public String Fcmtoken;
+}
